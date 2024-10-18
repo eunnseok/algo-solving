92b1f00c7a50ca880ee45ba9df9b633fe41d8cbc
eunnseok <167912024+eunnseok@users.noreply.github.com>
2024-10-18 22:04:07 +0900

    [Gold III] Title: 아기 상어, Time: 76 ms, Memory: 12228 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,151 +1,124 @@
-import java.util.*;
-import java.io.*;
 
-// BJ #16236 - 아기 상어
-// Strategy: BFS
+//백준 16236 아기 상어
+import java.awt.Point;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
-	static int N, r, c, size, time, cnt, tt;
-	static int[] fish;
-	static int[][] space;
-	static int[] dr = {-1,0,0,1};
-	static int[] dc = {0,-1,1,0};
-	
-	public static class Point{
-		int row;
-		int col;
-		
-		public Point(int row, int col) {
-			this.row = row;
-			this.col = col;
-		}
-	}
-	
-	public static void BFS() {
-		int rr = N;
-		int cc = N;
-		tt = Integer.MAX_VALUE;
-		
-		int[][] t = new int[N][N];
-		boolean[][] visited = new boolean[N][N];
-		Queue<Integer> q_x = new LinkedList<>();
-		Queue<Integer> q_y = new LinkedList<>();
-		q_x.add(r);
-		q_y.add(c);
-		visited[r][c] = true;
-		t[r][c] = 0;
-		
-		while(!q_x.isEmpty()) {
-			int tr = q_x.poll();
-			int tc = q_y.poll();
-			
-			if(t[tr][tc] >= tt) {
-				break;
-			}
-			
-				
-			for(int i=0; i<4; i++) {
-				int nr = tr + dr[i];
-				int nc = tc + dc[i];
-				
-				if(nr < 0 || nc < 0 || nr >= N || nc >= N || space[nr][nc] > size) {
-					continue;
-				}
-					
-				if(visited[nr][nc]) continue;
-				
-				if(space[nr][nc] != 0 && space[nr][nc] < size) {
-					if(nr < rr) {
-						rr = nr;
-						cc = nc;
-						tt = t[tr][tc] + 1;
-					}else if(nr == rr) {
-						if(nc < cc) {
-							rr = nr;
-							cc = nc;
-							tt = t[tr][tc] + 1;
-						}
-					}
-					continue;
-				}
-					
-				q_x.add(nr);
-				q_y.add(nc);
-				visited[nr][nc] = true;
-				t[nr][nc] = t[tr][tc] + 1;
-			}
-		}
-		
-		if(tt != Integer.MAX_VALUE) {
-			r = rr;
-			c = cc;
-			cnt++;
-			fish[space[rr][cc]]--;
-			space[rr][cc] = 0;
-			if(cnt == size) {
-				size++;
-				cnt = 0;
-			}
-			time += tt;
-		}
-		
-		
-		
-	}
-	
-	public static void Eat() {
-		time = 0;
-		size = 2;
-		cnt = 0;
-		
-		while(true) {
-			int num = 0;
-			if(size < 7) {
-				for(int i=1; i<size; i++) {
-					num += fish[i];
-				}
-			}else {
-				for(int i=1; i<7; i++) {
-					num += fish[i];
-				}
-			}
-			
-			
-			if(num == 0) break;
-			
-			BFS();
-			
-			if(tt == Integer.MAX_VALUE) break;
-			
-		}
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		
-		space = new int[N][N];
-		
-		fish = new int[7];
-		
-		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				space[i][j] = Integer.parseInt(st.nextToken());
-				if(space[i][j] == 9) {
-					r = i;
-					c = j;
-					space[i][j] = 0;
-				}else if(space[i][j] != 0) {
-					fish[space[i][j]]++;
-				}
-			}
-		}
+    static int n;
+    static int[][] fishBowl;
+    static boolean[][] visited;
+    static Point shark;
+    static int babyShark = 2;
+    static int time = 0;
+    static boolean flag = true;
+    static int eatCnt=0;
 
-		Eat();
-		
-		// 최종 결과 출력
-		System.out.println(time);
-	}
+    static class Point {
+        int r;
+        int c;
+
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        fishBowl = new int[n][n];
+
+        shark = new Point(-1, -1);
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                fishBowl[i][j] = Integer.parseInt(st.nextToken());
+                if (fishBowl[i][j] == 9) {
+                    shark.r = i;
+                    shark.c = j;
+                    fishBowl[i][j] = 0;
+                }
+            }
+        }
+        while (flag) {
+            bfs(shark.r, shark.c);
+//            for(int k=0; k<n; k++){
+//                System.out.println(Arrays.toString(fishBowl[k]));
+//
+//            }
+//            System.out.println("size: " + babyShark + " cnt: " + eatCnt + " time: " + time);
+//            System.out.println();
+        }
+        System.out.println(time);
+    }
+
+    public static void bfs(int r, int c) {
+//      System.out.println(time);
+        Queue<Point> queue = new LinkedList<>();
+        visited = new boolean[n][n];
+
+        int[] dr = { -1, 0, 0, 1 };
+        int[] dc = { 0, -1, 1, 0 };
+        int dist = 1;
+
+        queue.add(new Point(r, c));
+        visited[r][c] = true;
+
+        boolean eatFlag = false;
+        int er = 100;
+        int ec = 100;
+
+        while (!queue.isEmpty()) {
+            int qSize = queue.size();
+            while (qSize-- > 0) {
+                Point p = queue.poll();
+
+                for (int d = 0; d < 4; d++) {
+                    int nr = p.r + dr[d];
+                    int nc = p.c + dc[d];
+                    if (nr < 0 || nc < 0 || nr >= n || nc >= n || visited[nr][nc] || fishBowl[nr][nc] > babyShark) {
+                        continue;
+                    }
+                    if (fishBowl[nr][nc] < babyShark && fishBowl[nr][nc] != 0) {
+                        if(nr < er){
+                            er = nr;
+                            ec = nc;
+                        }else if(nr == er){
+                            if(nc < ec){
+                                er = nr;
+                                ec = nc;
+                            }
+                        }
+                    }
+                    if (fishBowl[nr][nc] == 0 || fishBowl[nr][nc] == babyShark) {
+                        queue.add(new Point(nr, nc));
+                        visited[nr][nc] = true;
+                    }
+                }
+            }
+
+            if(er != 100){
+                fishBowl[er][ec] = 0;
+                shark.r = er;
+                shark.c = ec;
+                time += dist;
+                flag = true;
+                eatCnt++;
+                if(eatCnt == babyShark) {
+                    babyShark++;
+                    eatCnt=0;
+                }
+                return;
+            }
+
+            dist++;
+        }
+        flag = false;
+        return;
+    }
+
 }
